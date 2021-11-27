9d6385d8b43b79aa9215dc949858393135a83bb1
Enrique De Nobrega <edenobrega@sky.com>
2021-11-27 15:38:39 +0000

    Add comments to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 /* jshint esversion: 8 */
+
+// Currently selected value
 let phraseTimed = false;
 document.getElementById("phrase-timed").addEventListener("change", function(){
+    // Flip the bool on change
     phraseTimed = !phraseTimed;
+    // Replace the old text value with the new one,
+    // flip the first param of the replace to match what it used to be
     document.getElementById("second-play").href = 
         document.getElementById("second-play").href.replace(`timed=${!phraseTimed}`,`timed=${phraseTimed}`);
 });
@@ -13,13 +18,18 @@ document.getElementById("word-timed").addEventListener("change", function(){
         document.getElementById("first-play").href.replace(`timed=${!timed}`,`timed=${timed}`);
 });
 
+// Currently selected value
 let currentWordFile = "words";
 document.getElementById("word-select").addEventListener("change", function(){
+    // Get the new select value
     let newFile = document.getElementById("word-select").value;
+    // Replace the old text value with the new one
     document.getElementById("first-play").href = 
         document.getElementById("first-play").href.replace(`file=${currentWordFile}`, `file=${newFile}`);
+    // Replace old value with the new
     currentWordFile = newFile;
 });
+
 
 let currentPhraseFile = "phrases";
 document.getElementById("phrase-select").addEventListener("change", function(){
