e4807d6bd37262e96579217db56e009aa9017fb1
Enrique De Nobrega <edenobrega@sky.com>
2021-11-29 18:17:34 +0000

    Improve modal.js, change modal from div to aside

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,28 +1,24 @@
 /* jshint esversion: 8 */
 
-// modal code from https://www.w3schools.com/howto/howto_css_modals.asp
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", function () {
+  modal.style.display = "block";
+});
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
+let span = document.getElementById("close");
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.addEventListener("click", function () {
   modal.style.display = "none";
-};
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.addEventListener("click",function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-  }
-};
+  }});
